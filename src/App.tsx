fe8fa6a4eed06e3424a6597b4fc58a79b93cb34e
puranban <puranban77@gmail.com>
2025-03-18 16:45:06 +0545

    feat: add route for page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { Flex, Layout } from 'antd';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
 import { PlayerProvider } from '#contexts/PlayerContext';
+import { TeamProvider } from '#contexts/TeamContext';
 import PlayerForm from '#views/PlayerForm';
-import { Layout } from 'antd';
+import TeamForm from '#views/TeamForm';
 
 import './App.css';
 
 function App() {
   return (
     <PlayerProvider>
-      <Layout className='layout'>
-      <PlayerForm />
-      </Layout>
+      <TeamProvider>
+        <Layout className='layout'>
+          <Router>
+            <nav>
+              <Flex gap={24}>
+                <Link to="/">Players</Link>
+                <Link to="/teams">Teams</Link>
+                <Link to="/generate">Generate Teams</Link>
+              </Flex>
+            </nav>
+            <Routes>
+              <Route path="/" element={<PlayerForm />} />
+              <Route path="/teams" element={<TeamForm />} />
+              {/* <Route path="/generate" element={<TeamGenerator />} /> */}
+            </Routes>
+          </Router>
+        </Layout>
+      </TeamProvider>
     </PlayerProvider>
   );
 }
